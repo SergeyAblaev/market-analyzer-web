48a608779580226d9536fcd52ff3ed7d1c3bc29a
Sergey.Ablaev <mailto:sergei.ablaev@soft-control.ru>
2026-07-13 01:03:46 +0700

    Added a new API foundation /api/v1/alertrules

--- backend/src/main/java/com/example/cryptoanalyzer/web/api/v1/AlertRulesV1Controller.java
+++ backend/src/main/java/com/example/cryptoanalyzer/web/api/v1/AlertRulesV1Controller.java
@@ -1,18 +1,22 @@
-package com.example.cryptoanalyzer.web;
+package com.example.cryptoanalyzer.web.api.v1;
 
 import com.example.cryptoanalyzer.rules.AlertRule;
 import com.example.cryptoanalyzer.rules.service.AlertRulesService;
 import com.example.cryptoanalyzer.web.model.AlertRuleUpdateDto;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/alertrules")
-@Deprecated
-public class AlertRulesController {
+@RequestMapping("/api/v1/alertrules")
+@Tag(name = "Alert Rules API v1")
+public class AlertRulesV1Controller {
 
     private final AlertRulesService service;
 
